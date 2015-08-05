ae6b6099785203ddcb019029c874b9cb642eaca3
Igor Chepurnoy <igorzfort@gmail.com>
2015-08-05 10:36:32 +0300

    Update form.php

--- enum/form.php
+++ enum/form.php
@@ -9,5 +9,6 @@ echo $form->field($generator, 'ns');
 echo $form->field($generator, 'author');
 echo $form->field($generator, 'description')->textarea(['style' => 'resize:vertical']);
 echo $form->field($generator, 'constValues')->textarea(['style' => 'resize:vertical']);
-
+echo $form->field($generator, 'start')->label('Start numbers from');
+echo $form->field($generator, 'sort')->checkbox(['label' => 'Sort values']);
 
